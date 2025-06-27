e81c5dcd49cb402e3abcd9179db4f1673d8a0aae
Manish Kumar Lath <manishlath3445@gmail.com>
2025-06-28 01:53:12 +0530

    Add speed insights for vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
